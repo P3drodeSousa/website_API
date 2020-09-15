401e686a741ea81c134a256c70efbecf1b2ccc03
tempname1533 <pedro_de_sousa@outlook.com>
2020-09-15 15:23:28 +0200

    make it better

--- server.js
+++ server.js
@@ -29,6 +29,7 @@ app.get("/", (req, res) => {
   res.send("Hello world");
 });
 
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Our app is running on port ${PORT}`);
+});
