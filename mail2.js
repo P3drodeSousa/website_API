ccc55ef34133a35deab0640face9dc06cb7b465d
tempname1533 <pedro_de_sousa@outlook.com>
2020-09-24 14:59:07 +0200

    Email changed

--- mail2.js
+++ mail2.js
@@ -6,7 +6,7 @@ sgMail.setApiKey(
 
 const msg = {
   to: "contact@pesousa.dev",
-  from: "test@example.com", // Use the email address or domain you verified above
+  from: "pedro_de_sousa@outlook.com", // Use the email address or domain you verified above
   subject: "Sending with Twilio SendGrid is Fun",
   text: "and easy to do anywhere, even with Node.js",
   html: "<strong>and easy to do anywhere, even with Node.js</strong>",
