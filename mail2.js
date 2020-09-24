d91c54a3890a2f63595d22f57c2e739c4194b2e9
tempname1533 <pedro_de_sousa@outlook.com>
2020-09-24 15:05:19 +0200

    Email changed

--- mail2.js
+++ mail2.js
@@ -4,18 +4,25 @@ sgMail.setApiKey(
   "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"
 );
 
-const msg = {
-  to: "contact@pesousa.dev",
-  from: "pedro_de_sousa@outlook.com", // Use the email address or domain you verified above
-  subject: "Sending with Twilio SendGrid is Fun",
-  text: "and easy to do anywhere, even with Node.js",
-  html: "<strong>and easy to do anywhere, even with Node.js</strong>",
-};
-
-//ES8
-
 const sendEmail = async (infos) => {
   const { email, name, message } = infos;
+
+  const msg = {
+    to: "contact@pesousa.dev",
+    from: "pedro_de_sousa@outlook.com", // Use the email address or domain you verified above
+    subject: "Vous avez un nouveau message de contact",
+    text: "Vous avez un nouveau message de contact",
+    html: `
+    <h3>Contact Details</h3>
+    <ul>  
+      <li>Nom: ${name}</li>
+      <li>Email: ${email}</li>
+    </ul>
+    <h3>Message</h3>
+    <p>${message}</p>
+  `,
+  };
+
   try {
     await sgMail.send(msg);
     return { ok: true };
